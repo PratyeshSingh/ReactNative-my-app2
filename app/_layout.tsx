4bb941746c8403176b35d8281daeaadb44e31df9
pratyesh <pratyesh3@gmail.com>
2026-07-23 16:04:17 +0530

    minor chanegs to setup title for each pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack >
+    <Stack.Screen name="index" options={{ title: 'Home' }} />;
+    <Stack.Screen name="product_list" options={{ title: 'Products' }} />;
+    <Stack.Screen name="product_detail" options={{ title: 'Product Detail' }} />;
+    <Stack.Screen name="about_me" options={{ title: 'About Me' }} />;
+    <Stack.Screen name="user-screen" options={{ title: 'Profile' }} />;
+  </Stack>;
 }
