7e93d17052435100f2d5fd8a335d84318031805a
pratyesh <pratyesh3@gmail.com>
2026-08-21 09:52:36 +0530

    nit changes

--- app/about_me.tsx
+++ app/about_me.tsx
@@ -1,13 +1,9 @@
-import { useEffect, useState } from 'react';
 import { Text, View } from "react-native";
 import styles from "../src/style";
 
 const About = () => {
-    const [architectureDetails, setArchitectureDetails] = useState<string | null>(null);
-    useEffect(() => {
-        const isFabric = !!(globalThis as any).nativeFabricUIManager;
-        setArchitectureDetails("Is Fabric & Herms / New Arch Enabled? " + (isFabric ? "Yes ✅" : "No ❌"));
-    }, []);
+    const isFabric = !!(globalThis as any).nativeFabricUIManager;
+    const architectureDetails = "Is Fabric & Hermes / New Arch Enabled? " + (isFabric ? "Yes ✅" : "No ❌");
 
     return (
         <View style={styles.textViewStyle}>
